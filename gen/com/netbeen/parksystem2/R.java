1f6cba801a1c2b93ae633c951bce0ccbf783fcc8
NetBeen <netbeen.cn@gmail.com>
2015-05-04 09:09:57 +0800

    login avtivity compete.

--- gen/com/netbeen/parksystem2/R.java
+++ gen/com/netbeen/parksystem2/R.java
@@ -10,16 +10,43 @@ package com.netbeen.parksystem2;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int themeBackground=0x7f040001;
+        public static final int themeColor=0x7f040000;
+        public static final int white=0x7f040002;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int login_bg_down=0x7f020001;
+        public static final int login_bg_up=0x7f020002;
+        public static final int login_btn=0x7f020003;
+        public static final int login_btn_press=0x7f020004;
+        public static final int login_button_selector=0x7f020005;
+        public static final int pw_icon=0x7f020006;
+        public static final int user_icon=0x7f020007;
+    }
+    public static final class id {
+        public static final int loginButton=0x7f070003;
+        public static final int loginTitle=0x7f070000;
+        public static final int mainTableHost=0x7f070005;
+        public static final int otherLoginFunction=0x7f070004;
+        public static final int passwordEditText=0x7f070002;
+        public static final int usernameEditText=0x7f070001;
     }
     public static final class layout {
         public static final int login=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int title_activity_login=0x7f040002;
+        public static final int app_name=0x7f050000;
+        public static final int forgetPasswordText=0x7f050005;
+        public static final int hello_world=0x7f050008;
+        public static final int loginButtonText=0x7f050004;
+        public static final int passwordHint=0x7f050003;
+        public static final int registNowText=0x7f050006;
+        public static final int title_activity_login=0x7f050001;
+        public static final int title_activity_main=0x7f050007;
+        public static final int usernameHint=0x7f050002;
     }
     public static final class style {
         /** 
@@ -43,10 +70,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- src/com/netbeen/parksystem2/LoginActivity.java
+++ src/com/netbeen/parksystem2/LoginActivity.java
@@ -2,14 +2,30 @@ package com.netbeen.parksystem2;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.content.Intent;
 
 public class LoginActivity extends Activity {
+	private Button loginButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
+		
+		loginButton = (Button)this.findViewById(R.id.loginButton);
+		loginButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent();
+				intent.setClass(LoginActivity.this, MainActivity.class);
+				startActivity(intent);
+				LoginActivity.this.finish();
+			}
+		});
+		
 	}
 }
